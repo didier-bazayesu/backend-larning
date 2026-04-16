d78d6876216785bf446f0559330098bca7e500f4
didier-bazayesu <didierbazayesu@gmail.com>
2026-04-16 11:43:47 +0200

    feat: adding and deleting the files and directories

--- backend.js
+++ backend.js
@@ -1,12 +1,13 @@
+const { error } = require('console');
 const fs = require('fs');
 
 //here is the way of writing on the file 
 
-// fs.writeFile('C:\\Users\\user\\Documents\\The gym\\react-understanding\\react-project\\backend-larning\\written.js', "const name ='didier'",(error)=>{
-//    if(error) console.log("An error occurred while writing the file:", error);
-//    else
-//     console.log("File has been written successfully.");
-// })
+fs.writeFile('C:\\Users\\user\\Documents\\The gym\\react-understanding\\react-project\\backend-larning\\written.js', "const name ='didier'",(error)=>{
+   if(error) console.log("An error occurred while writing the file:", error);
+   else
+    console.log("File has been written successfully.");
+})
 
 
 //reading the file 
@@ -18,3 +19,33 @@ fs.readFile(path,(error,data)=>{
      else console.log(data.toString());
      console.log("File has been read successfully.");
 })
+
+
+
+//directories 
+if(fs.existsSync(`C:\\Users\\user\\Documents\\The gym\\react-understanding\\react-project\\backend-larning\\newDirectory`)){
+    console.log("Directory already exists.");
+}   
+fs.mkdir(`C:\\Users\\user\\Documents\\The gym\\react-understanding\\react-project\\backend-larning\\newDirectory`,(error)=>{
+    if(error) console.log("An error occurred while creating the directory:", error);
+    else console.log("Directory has been created successfully.");
+})
+
+// //deleting the directory
+if(fs.existsSync(`C:\\Users\\user\\Documents\\The gym\\react-understanding\\react-project\\backend-larning\\newDirectory`)){
+    fs.rmdir(`C:\\Users\\user\\Documents\\The gym\\react-understanding\\react-project\\backend-larning\\newDirectory`,(error)=>{
+        if(error) console.log("An error occurred while deleting the directory:", error);
+        else console.log("Directory has been deleted successfully.");
+    })
+    
+} else console.log("Directory does not exist.");
+
+
+
+//delete the file
+if(fs.existsSync(path)){
+     fs.unlink(path,(error)=>{
+        if(error) console.log("An error occurred while deleting the file:", error);
+        else console.log("File has been deleted successfully."); 
+    })
+}else console.log("File does not exist.");
