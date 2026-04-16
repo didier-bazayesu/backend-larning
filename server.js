53377dc44f56cb821f076b5664748f3fae4ccaca
didier-bazayesu <didierbazayesu@gmail.com>
2026-04-16 14:13:14 +0200

    feat : navigating through the link

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'});
-    fs.readFile("./view/index.html",(error,information)=>{
-        if(error) {
-            console.log("An error occurred while reading the file:", error);
-            res.writeHead(500,{'Content-Type':'text/plain'});
-            res.write("Internal Server Error");
-            res.end();
-        }
-        else {
-           if(req.url === "/"){
-            res.write(information);
-            res.end();
-           } else if(req.url === "/about"){
-            res.write("<h1>About Page</h1><p>This is the about page of our simple Node.js server.</p>");
-            res.end();  
-           }
-        }
-    })
-
+const server = http.createServer((req, res) => {
 
     console.log(req.url);
 
-})
+    // 🔁 Redirect
+    if (req.url === '/about-me') {
+        res.setHeader('Location', '/about');
+        res.writeHead(302);
+        return res.end(); // ⛔ STOP HERE
+    }
 
-server.listen(5000,'localhost',()=>{
-    console.log("the server is running")
-})
+    // 📄 Choose file
+    const path =
+        req.url === '/' ? './view/index.html' :
+        req.url === '/about' ? './view/about.html' :
+        './view/404.html';
+
+    fs.readFile(path, (error, data) => {
+        if (error) {
+            res.writeHead(500, { 'Content-Type': 'text/html' });
+            return res.end("<h1>Internal Server Error</h1>");
+        }
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+
+});
+
+server.listen(5000, 'localhost', () => {
+    console.log("the server is running");
+});
