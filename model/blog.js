468241c6c092dcaff3f34813c4d90c32a17147e2
didier-bazayesu <didierbazayesu@gmail.com>
2026-04-20 19:01:31 +0200

    feat: model for saving on the database

--- model/blog.js
+++ model/blog.js
@@ -1,2 +1,22 @@
 const mangoose = require('mongoose');
-const schema = mangoose.Schema;
+const Schema = mangoose.Schema;
+
+const blogSchema = new Schema ({
+    title:{
+        type:String,
+        required : true,
+    },
+    snippet:{
+        type:String,
+        required : true,
+        },
+    body:{
+        type:String,
+        required : true,
+    }   
+},{timestamps:true})  
+
+const Blog = mangoose.model("didier_collection",blogSchema);
+
+//exporting the model to be used in other files
+module.exports = Blog;
